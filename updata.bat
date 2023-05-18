45ad4a0450cae26e4c979d9efd38c33895d6973d
lanzhi6 <lan_zhi@outlook.com>
2023-05-18 23:58:09 +0800

    Update BluestarAPI:3.4.6-pre1

--- updata.bat
+++ updata.bat
@@ -1,15 +1,32 @@
 @echo off
-set /p project=Project Name:
-set /p version=Version:
+
+if not "%1"=="" set project=%1
+if not "%2"=="" set version=%2
+
+if "%project%"=="" set /p project=Please enter the project name:
+if "%version%"=="" set /p version=Please enter the project version:
+
+:pull
+git pull
+
+if errorlevel 1 (
+    echo Pull failed! Retrying...
+    timeout /t 5 > nul
+    goto pull
+) else (
+    echo Pull successful!
+)
+
+git add -A
+git commit -m "Update %project%:%version%"
 
 :push
-git add -A
-git commit -m "Upload %project%:%version% to GitHub"
 git push
 
 if errorlevel 1 (
-    echo 推送失败！正在重试...
+    echo Push failed! Retrying...
+    timeout /t 5 > nul
     goto push
 ) else (
-    echo 推送成功!
+    echo Push successful!
 )
